f93decc91ec7f75e73b75eccb84a39dced80f320
Kieren <Kieren@JUIPTER>
2016-08-09 14:03:13 +1000

    wip csv to database script

---
+++ PX_maps/Controllers/DatabaseController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualBasic.FileIO;
+using PX_maps.Models;
+using System.Configuration;
+
+namespace PX_maps.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        // GET: Database
+        public ActionResult Index()
+        {
+            // update database 
+            return View();
+        }
+
+        private void GetDataTabletFromCSVFile(string csv_file_path)
+        {
+            DataTable csvData = new DataTable();
+            try
+            {
+                using (TextFieldParser csvReader = new TextFieldParser(csv_file_path))
+                {
+                    csvReader.SetDelimiters(new string[] { "," });
+                    csvReader.HasFieldsEnclosedInQuotes = false;
+                    string[] colFields = csvReader.ReadFields();
+                    foreach (string column in colFields)
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        csvData.Columns.Add(datecolumn);
+                    }
+                    while (!csvReader.EndOfData)
+                    {
+                        string[] fieldData = csvReader.ReadFields();
+                        for (int i = 0; i < fieldData.Length; i++)
+                        {
+                            if (fieldData[i] == "")
+                            {
+                                fieldData[i] = null;
+                            }
+                        }
+                        csvData.Rows.Add(fieldData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            InsertDataIntoSQLServerUsingSQLBulkCopy(csvData);
+        }
+        static void InsertDataIntoSQLServerUsingSQLBulkCopy(DataTable csvFileData)
+        {
+            using (SqlConnection sql = new SqlConnection(GPSDBContext.ConnectionString))
+            {
+                dbConnection.Open();
+                using (SqlBulkCopy s = new SqlBulkCopy(dbConnection))
+                {
+                    s.DestinationTableName = "GPS Data";
+                    foreach (var column in csvFileData.Columns)
+                        s.ColumnMappings.Add(column.ToString(), column.ToString());
+                    s.WriteToServer(csvFileData);
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+
+                GetDataTabletFromCSVFile(path);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ PX_maps/Models/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PX_maps.Models
+{
+    public class Database
+    {
+        public string deviceId { get; set; }
+        public int latitude { get; set; }
+        public int longitude { get; set; }
+        public int speed { get; set; }
+        public int reliability { get; set; }
+        public int satellite { get; set; }
+        public string type { get; set; }
+        public int locked { get; set; }
+        public string date { get; set; }
+    }
+    public class GPSDBContext : DbContext
+    {
+        public DbSet<Database> GPSData { get; set; }
+    }
+}
