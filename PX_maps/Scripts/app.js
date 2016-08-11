22ac25324e62f8a8d69ca56683b4f81ebc12b951
Fidel Torres <fidel@guaroco.com>
2016-08-11 13:21:29 +1000

    Added the side menus for Visualization and Map Control

---
+++ PX_maps/Scripts/app.js
@@ -0,0 +1,24 @@
+﻿var menuVisualization = document.getElementById('pxm-sideMenu-visualization');
+var menuMaps = document.getElementById('pxm-sideMenu-maps');
+
+showVisualizationMenu.onclick = function () {
+	classie.toggle(this, 'active');
+	classie.toggle(menuVisualization, 'pxm-sideMenu-open');
+	disableOther('showVisualizationMenu');
+}
+
+showMapsMenu.onclick = function () {
+	classie.toggle(this, 'active');
+	classie.toggle(menuMaps, 'pxm-sideMenu-open');
+	disableOther('showMapsMenu');
+}
+
+
+function disableOther(btn) {
+	if (btn !== 'showVisualizationMenu') {
+		classie.toggle(showVisualizationMenu, 'disable');
+	}
+	if (btn !== 'showMapsMenu') {
+		classie.toggle(showMapsMenu, 'disable');
+	}
+}
