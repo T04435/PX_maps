61b0d74976e9f2a81d0c9169e66b204579e6f655
Fidel Torres <fidel@guaroco.com>
2016-09-27 13:32:51 +1000

    wip

--- PX_maps/Controllers/MapController.cs
+++ PX_maps/Controllers/MapController.cs
@@ -12,13 +12,13 @@ using System.Web.Script.Serialization;
 
 namespace PX_maps.Controllers
 {
-    public class MapController : Controller
-    {
-        // GET: GoogleMap
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class MapController : Controller
+	{
+		// GET: GoogleMap
+		public ActionResult Index()
+		{
+			return View();
+		}
 
 		public ActionResult SnapLanes()
 		{
@@ -26,40 +26,43 @@ namespace PX_maps.Controllers
 		}
 
 		public JsonResult getListLatLongs(string hour, string day)
-        {
-            SqlDataReader dr;
-            List<LatLong> dbCoordinates = new List<LatLong>();
+		{
+			SqlDataReader dr;
+			List<LatLong> dbCoordinates = new List<LatLong>();
 
-            using (SqlConnection dbconn = new SqlConnection(ConfigurationManager.ConnectionStrings["GPSDBContext"].ConnectionString))
-            {
-                string CommandText = "SELECT latitude,longitude FROM Data WHERE DATENAME(WEEKDAY,[isoDate]) = @weekday AND DATEPART(HOUR,[isoDate]) = @hour";
-                string CommandText = "SELECT [deviceId~2],latitude,longitude,[speed~1] FROM Test WHERE DATENAME(WEEKDAY,[isoDate~9]) = @weekday AND DATEPART(HOUR,[isoDate~9]) = @hour ORDER BY [deviceId~2], [isoDate~9]";
+			using (SqlConnection dbconn = new SqlConnection(ConfigurationManager.ConnectionStrings["GPSDBContext"].ConnectionString))
+			{
+				/*
+				 * Use this one to get the count of different devices in the query
+				 * string CommandText = SELECT COUNT(DISTINCT deviceId) AS deviceCount FROM Data [SAME QUERY]";
+				*/
+				string CommandText = "SELECT deviceId,latitude,longitude,speed FROM Data WHERE DATENAME(WEEKDAY,[isoDate]) = @weekday AND DATEPART(HOUR,[isoDate]) = @hour  AND speed !=0 ORDER BY [deviceId], [isoDate]";
 
-                using (SqlCommand cmd = new SqlCommand(CommandText, dbconn))
-                {
-                    cmd.Parameters.AddWithValue("@weekday", day);
-                    cmd.Parameters.AddWithValue("@hour", hour);
-                    dbconn.Open();
-                    dr = cmd.ExecuteReader();
-                    if (dr.HasRows)
-                    {
-                        while (dr.Read())
-                        {
-                            dbCoordinates.Add(new LatLong
-                            {
-                                deviceID = dr["deviceId~2"].ToString(),
-                                speed = Decimal.Parse(dr["speed~1"].ToString()),
-                                latitude = Decimal.Parse(dr["latitude"].ToString()),
-                                longitude = Decimal.Parse(dr["longitude"].ToString())
-                            });
-                        }
-                    }
-                }
-                dbconn.Close();
-            }
-            JsonResult result = Json(dbCoordinates, JsonRequestBehavior.AllowGet);
-            result.MaxJsonLength = 8675309;
-            return result;
-        }
-    }
+				using (SqlCommand cmd = new SqlCommand(CommandText, dbconn))
+				{
+					cmd.Parameters.AddWithValue("@weekday", day);
+					cmd.Parameters.AddWithValue("@hour", hour);
+					dbconn.Open();
+					dr = cmd.ExecuteReader();
+					if (dr.HasRows)
+					{
+						while (dr.Read())
+						{
+							dbCoordinates.Add(new LatLong
+							{
+								deviceID = dr["deviceId"].ToString(),
+								speed = Decimal.Parse(dr["speed"].ToString()),
+								latitude = Decimal.Parse(dr["latitude"].ToString()),
+								longitude = Decimal.Parse(dr["longitude"].ToString())
+							});
+						}
+					}
+				}
+				dbconn.Close();
+			}
+			JsonResult result = Json(dbCoordinates, JsonRequestBehavior.AllowGet);
+			result.MaxJsonLength = 8675309;
+			return result;
+		}
+	}
 }
