4eb3b048b4170cbbb5278790fd4b7cd42c109358
Fidel Torres <fidel@guaroco.com>
2016-10-12 21:44:17 +1100

    Added the area speed average. to the map

--- PX_maps/Controllers/MapController.cs
+++ PX_maps/Controllers/MapController.cs
@@ -12,13 +12,13 @@ using System.Web.Script.Serialization;
 
 namespace PX_maps.Controllers
 {
-    public class MapController : Controller
-    {
-        // GET: GoogleMap
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class MapController : Controller
+	{
+		// GET: GoogleMap
+		public ActionResult Index()
+		{
+			return View();
+		}
 
         public JsonResult getListLatLongs(string hour, string day)
         {
@@ -29,6 +29,33 @@ namespace PX_maps.Controllers
             {
                 string CommandText = "SELECT [deviceId],latitude,longitude,[speed] FROM Data WHERE DATENAME(WEEKDAY,[isoDate]) = @weekday AND DATEPART(HOUR,[isoDate]) = @hour AND speed > 0 ORDER BY [deviceId], [isoDate]";
 
+				using (SqlCommand cmd = new SqlCommand(CommandText, dbconn))
+				{
+					cmd.Parameters.AddWithValue("@weekday", day);
+					cmd.Parameters.AddWithValue("@hour", hour);
+					dbconn.Open();
+					dr = cmd.ExecuteReader();
+					if (dr.HasRows)
+					{
+						while (dr.Read())
+						{
+							dbCoordinates.Add(new LatLong
+							{
+								deviceID = dr["deviceId"].ToString(),
+								speed = Decimal.Parse(dr["speed"].ToString()),
+								latitude = Decimal.Parse(dr["latitude"].ToString()),
+								longitude = Decimal.Parse(dr["longitude"].ToString())
+							});
+						}
+					}
+				}
+				dbconn.Close();
+			}
+			JsonResult result = Json(dbCoordinates, JsonRequestBehavior.AllowGet);
+			result.MaxJsonLength = 8675309;
+			return result;
+		}
+	}
                 using (SqlCommand cmd = new SqlCommand(CommandText, dbconn))
                 {
                     cmd.Parameters.AddWithValue("@weekday", day);
