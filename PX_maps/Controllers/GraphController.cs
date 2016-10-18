5a75f7c0f824d7478a84a009644b23d10ca635df
Fidel Torres <fidel@guaroco.com>
2016-10-18 21:05:02 +1100

    adding MVC for graph

---
+++ PX_maps/Controllers/GraphController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PX_maps.Controllers
+{
+	public class GraphController : Controller
+	{
+		// GET: Graph
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public List<Int32> getAVGDailySpeed(string day)
+		{
+			List<Int32> dailyAvgSpeed = new List<int>();
+			SqlDataReader dr;
+
+			using (SqlConnection dbconn = new SqlConnection(ConfigurationManager.ConnectionStrings["GPSDBContext"].ConnectionString))
+			{
+				//string CommandText = "SELECT (SUM(speed)/COUNT(speed)) FROM Data WHERE DATENAME(WEEKDAY,[isoDate]) = @weekday AND speed > 0 GROUP BY AND DATEPART(HOUR,[isoDate]) = @hour ";
+				string CommandText = "SELECT AVG(speed) AS hourlyAVGSpeed FROM Data WHERE DATENAME(WEEKDAY,[isoDate]) = @weekday AND speed > 0 GROUP BY DATEPART(HOUR,[isoDate]) ";
+
+				using (SqlCommand cmd = new SqlCommand(CommandText, dbconn))
+				{
+					cmd.Parameters.AddWithValue("@weekday", day);
+					dbconn.Open();
+					dr = cmd.ExecuteReader();
+					if (dr.HasRows)
+					{
+						while (dr.Read())
+						{
+							dailyAvgSpeed.Add(Int32.Parse(dr["hourlyAVGSpeed"].ToString()));
+						}
+					}
+				}
+				dbconn.Close();
+			}
+
+			return dailyAvgSpeed;
+		}
+	}
+}
